579b3588da2c98a9519509fb7a13bec341f348d9
wnsgur4322 <32208343+wnsgur4322@users.noreply.github.com>
2021-05-17 16:01:02 -0700

    5/17 file upload class

--- file-storage/models/image.js
+++ file-storage/models/image.js
@@ -1,4 +1,5 @@
-const { ObjectId } = require('mongodb');
+const fs = require('fs');
+const { ObjectId, GridFSBucket } = require('mongodb');
 
 const { getDBReference } = require('../lib/mongo');
 
@@ -9,13 +10,45 @@ exports.saveImageInfo = async function (image) {
   return result.insertedId;
 };
 
+exports.saveImageFile = (image) => {
+  return new Promise((resolve, reject) => {
+    const db = getDBReference();
+    const bucket = new GridFSBucket(db, { bucketName: 'images' });
+    const metadata = {
+      contentType: image.contentType,
+      userId: image.userId
+    };
+
+    const uploadStream = bucket.openUploadStream(
+      image.filename,
+      { metadata: metadata }
+    );
+    fs.createReadStream(image.path).pipe(uploadStream)
+    .on('error', (err) => {
+      reject(err)
+    })
+    .on('finish', (result) => {
+      resolve(result._id);
+    });
+  });
+};
+
+exports.getImageDownloadStreamByFilename = (filename) => {
+  const db = getDBReference();
+  const bucket = new GridFSBucket(db, { bucketName: 'images' });
+  return bucket.openDownloadStreamByName(filename);
+};
+
 exports.getImageInfoById = async function (id) {
   const db = getDBReference();
-  const collection = db.collection('images');
+  // const collection = db.collection('images');
+  const bucket = new GridFSBucket(db, { bucketName: 'images' });
   if (!ObjectId.isValid(id)) {
     return null;
   } else {
-    const results = await collection.find({ _id: new ObjectId(id) })
+    // const results = await collection.find({ _id: new ObjectId(id) })
+    //   .toArray();
+    const results = await bucket.find({ _id: new ObjectId(id) })
       .toArray();
     return results[0];
   }
--- file-storage/server.js
+++ file-storage/server.js
@@ -3,7 +3,10 @@ const multer = require('multer');
 const crypto = require('crypto');
 
 const { connectToDB } = require('./lib/mongo');
-const { getImageInfoById } = require('./models/image');
+const { getImageInfoById,
+  saveImageInfo,
+  saveImageFile,
+  getImageDownloadStreamByFilename } = require('./models/image');
 const { createPool } = require('mysql2/promise');
 
 const app = express();
@@ -30,29 +33,77 @@ const upload = multer({
                                                         // falsey can be false, but not the same 
 
   }
-})
+});
+
+//app.use('/media/images', express.static(`${__dirname}/uploads`));
+
+
 
 app.get('/', (req, res, next) => {
   res.status(200).sendFile(__dirname + '/index.html');
 });
 
-app.post('/images', upload.single('image'), (req, res, next) => {
+app.post('/images', upload.single('image'), async (req, res, next) => {
   console.log("== req.body:", req.body);
   console.log("== req.file:", req.file);
-  res.status(200).send();
+  if (req.file && req.body && req.body.userId) {
+    const image = {
+      contentType: req.file.mimetype,
+      filename: req.file.filename,
+      path: req.file.path,
+      userId: req.body.userId
+    };
+    try {
+      // const id = await saveImageInfo(image);
+      const id = await saveImageFile(image);
+      res.status(200).send({
+        id: id
+      });
+    } catch (err) {
+      next(err);
+    } 
+  } else {
+    res.status(400).send({
+      error: "Request body must contain 'image' and 'userId' "
+    });
+  }
 });
 
 app.get('/images/:id', async (req, res, next) => {
   try {
     const image = await getImageInfoById(req.params.id);
     if (image) {
-      res.status(200).send(image);
+      // delete image.path;
+      // image.url = `/media/images/${image.filename}`;
+      const responseBody = {
+        _id: image._id,
+        filename: image.filename,
+        url: `/media/images/${image.filename}`,
+        contentType: image.metadata.contentType,
+        userId: image.metadata.userId
+      }
+      res.status(200).send(responseBody);
     } else {
       next();
     }
   } catch (err) {
     next(err);
   }
+});
+
+app.get('/media/images/:filename', (req, res, next) => {
+  getImageDownloadStreamByFilename(req.params.filename)
+    .on('file', (file) => {
+      res.status(200).type(file.metadata.contentType);
+    })
+    .on('error', (err) => {
+      if (err.code === 'ENOENT') {
+        next();
+      } else {
+        next(err);
+      }
+    })
+    .pipe(res);
 });
 
 app.use('*', (err, req, res, next) => {
